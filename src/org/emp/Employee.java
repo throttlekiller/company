716a77ce8512d15eb9da56d2325caeb6513c4938
throttlekiller <sanjeevi1203@gmail.com>
2022-01-21 11:33:39 +0530

    143

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,17 @@
+package org.emp;
+
+public class Employee {
+	private void employee() {
+		System.out.println("employee name is hari");
+
+	}
+	private void empid() {
+		System.out.println("emp id is 1234");
+
+	}
+	public static void main(String[] args) {
+		Employee e=new Employee();
+		e.employee();
+		e.empid();
+	}
+}
